ade9fcda87ef9be16a1797f7bb9af060ce60f18e
Luis Enrique <utp0138573@alumno.utpuebla.edu.mx>
2022-07-22 03:05:17 -0500

    notificacion

---
+++ Notificacion/Notificacion/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.LocalNotification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notificacion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var notificacion = new NotificationRequest
+            {
+                BadgeNumber = 1,
+                Description = "Prueba de notificacion",
+                Title = "Prueba notificacion",
+                ReturningData = "Dummy data",
+                NotificationId = 1337,
+                NotifyTime = DateTime.Now.AddSeconds(5) // Used for Scheduling local notification, if not specified notification will show immediately.
+            };
+            NotificationCenter.Current.Show(notificacion);
+        }
+    }
+}
